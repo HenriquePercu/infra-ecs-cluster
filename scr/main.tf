47b92619ac62e075b58803b4a90d205887298979
henriquepercudeoliveira <henrique.percu@gmail.com>
2023-03-28 21:36:42 -0300

    Fixing pipelines

--- scr/main.tf
+++ scr/main.tf
@@ -1,25 +1,3 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-    random = {
-      source  = "hashicorp/random"
-      version = "3.4.3"
-    }
-  }
-  required_version = ">= 1.1.0"
-
-  cloud {
-    organization = "private-henrique-percu"
-
-    workspaces {
-      name = "github-actions"
-    }
-  }
-}
-
 resource "aws_ecs_cluster" "demo_ecs_cluster" {
   lifecycle {
     create_before_destroy = true
