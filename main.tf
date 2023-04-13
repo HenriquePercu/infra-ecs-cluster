7766cb0906cc0e784073cb778cbb1c1dd763f344
henriquepercudeoliveira <henrique.percu@gmail.com>
2023-04-12 21:21:51 -0300

    Creating ECR

--- main.tf
+++ main.tf
@@ -10,3 +10,17 @@ resource "aws_ecs_cluster" "demo_ecs_cluster" {
     Name = "demo_ecs_cluster"
   }
 }
+
+
+resource "aws_ecr_repository" "demo_ecr" {
+  name                 = var.aws_ecr_repository_name
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = {
+    Env  = "demo"
+    Name = "demo_ecs_cluster"
+  }
+}
---
+++ variables.tf
@@ -0,0 +1,5 @@
+variable "aws_ecr_repository_name" {
+  type        = string
+  description = "ECR repository name"
+  default     = "demo_ecs_repository"
+}
